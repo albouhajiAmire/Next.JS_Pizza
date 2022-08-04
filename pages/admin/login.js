d7d3c104f1da6152efd1e303ae1bfa2be55ecf31
albouhajiDev <albouhajidev@gmail.com>
2022-08-04 20:40:02 +0000

    login:name =>admin,passw:123456

---
+++ pages/admin/login.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import styles from "../../styles/Login.module.css";
+// import 'react-toastify/dist/ReactToastify.css'; 
+// toast.configure()
+
+const Login = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [error, setError] = useState(false);
+  const router = useRouter();
+
+  const handleClick = async () => {
+    try {
+      await axios.post("http://localhost:3000/api/login", {
+        username,
+        password,
+      });
+      router.push("/admin");
+      // toast.succes("succes");
+
+    } catch (err) {
+      setError(true);
+      // toast.error("Une erreur");
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1>Admin Dashboard</h1>
+        <input
+          placeholder="username"
+          className={styles.input}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          className={styles.input}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleClick} className={styles.button}>
+          Sign In
+        </button>
+        {error && <span className={styles.error}>Wrong Credentials!</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ pages/product/[id].js
@@ -0,0 +1,110 @@
+import styles from "../../styles/Product.module.css";
+import Image from "next/image";
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../../redux/cartSlice";
+
+const Product = ({pizza}) => {
+  const [price, setPrice] = useState(pizza.prices[0]);
+  const [size, setSize] = useState(0);
+  const [extras, setExtras] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
+  
+  const changePrice = (number) => {
+    setPrice(price + number)
+  }
+  const handleSize = (sizeIndex)=>{
+    const difference = pizza.prices[sizeIndex] - pizza.prices[size] ;
+    setSize(sizeIndex);
+    changePrice(difference);
+  }
+
+  const handleChange = (e, option) =>{
+    const checked = e.target.checked;
+
+    if(checked){
+      changePrice(option.price);
+      //hna ghadi nkhdmo btari9at tasfia : ila makanch had l3onsor w lmo3araf mamsawinch fahtafd bihom ldakhl wila kano msawin fahydhom bstikhdam filter
+      setExtras((prev) =>[ ...prev, option])
+    }else{
+      changePrice(-option.price);
+      setExtras(extras.filter((extra) => extra._id !== option._id ))
+    }
+  }
+
+  const handleClickAddCart = () => {
+   dispatch(addProduct({...pizza,extras,price,quantity}))
+  }
+  // const pizza = {
+  //   id: 1,
+  //   img: "/img/pizza.png",
+  //   name: "CAMPAGNOLA",
+  //   price: [19.9, 23.9, 27.9],
+  //   desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu purus, rhoncus fringilla vestibulum vel, dignissim vel ante. Nulla facilisi. Nullam a urna sit amet tellus pellentesque egestas in in ante.",
+  // };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.imgContainer}>
+          <Image src={pizza.img} objectFit="contain" layout="fill" alt="" />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <h1 className={styles.title}>{pizza.title}</h1>
+        <span className={styles.price}>${price}</span>
+        <p className={styles.desc}>{pizza.desc}</p>
+        <h3 className={styles.choose}>Choose the size</h3>
+        <div className={styles.sizes}>
+          <div className={styles.size} onClick={() => handleSize(0)}>
+            <Image src="/img/size.png" layout="fill" alt="" />
+            <span className={styles.number}>Small</span>
+          </div>
+          <div className={styles.size} onClick={() => handleSize(1)}>
+            <Image src="/img/size.png" layout="fill" alt="" />
+            <span className={styles.number}>Medium</span>
+          </div>
+          <div className={styles.size} onClick={() => handleSize(2)}>
+            <Image src="/img/size.png" layout="fill" alt="" />
+            <span className={styles.number}>Large</span>
+          </div>
+        </div>
+        <h3 className={styles.choose}>Choose additional ingredients</h3>
+        <div className={styles.ingredients}>
+        {pizza.extraOptions.map((option)=> ( 
+          <div className={styles.option} key={option._id}>
+          <input
+          type="checkbox"
+          id={option.text}
+          name={option.text}
+          className={styles.checkbox}
+          onChange = {(e)=> handleChange(e,option)} 
+        />
+        <label htmlFor="double">{option.text}</label>
+        </div>
+        ))}
+        </div>
+        <div className={styles.add}>
+            <input onChange={(e)=> setQuantity(e.target.value)} type="number" defaultValue={1} className={styles.quantity}/>
+            <button className={styles.button} onClick={handleClickAddCart}>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export const getServerSideProps = async ({ params }) => {
+  const res = await axios.get(
+    `http://localhost:3000/api/products/${params.id}`
+  );
+  return {
+    props: {
+      pizza: res.data,
+    },
+  };
+};
+
+export default Product;
